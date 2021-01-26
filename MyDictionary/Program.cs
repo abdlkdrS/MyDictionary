9ae30ba98bb644f8f3d96f8d87b8010b5655efc6
Abdulkadirr <Abdulkadirr@ABDULKADIR>
2021-01-26 23:43:13 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Customer1");
+            Console.WriteLine(myDictionary.Length);
+            myDictionary.Add("Customer2");
+            Console.WriteLine(myDictionary.Length);
+            myDictionary.Add("Customer3");
+            Console.WriteLine(myDictionary.Length);
+
+            foreach (var item in myDictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
